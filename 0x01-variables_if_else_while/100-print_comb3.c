75ec99c2b38e41b82a9fc6d314e7447857a0890c
Tessy_James <tessyjames28@gmail.com>
2022-09-09 20:12:30 -0700

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i < 58; i += 1)
+	{
+		for (j = 48; j < 58; j += 1)
+		{
+			if (i != j && i < j)
+			{
+				putchar(i);
+				putchar(j);
+				if (i == 56 && j == 57)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
