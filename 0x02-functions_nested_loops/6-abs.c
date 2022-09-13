6bd3e1dfc6af06250cbe55467d1e6f7b0b128b21
Tessy_James <tessyjames28@gmail.com>
2022-09-13 16:45:57 -0700

    Tasks 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - main function to check absolute value
+ * @n: prototype type
+ * Return: Always 0
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		return (n * -1);
+	else
+		return (n);
+	return (0);
+}
