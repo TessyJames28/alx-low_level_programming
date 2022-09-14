9357e1cc2c4ca55e43a75586dba8550a69d24fa0
Tessy_James <tessyjames28@gmail.com>
2022-09-13 19:37:39 -0700

    Tasks 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - main function
+ * @i: first value
+ * @j: second value
+ * Return: Always 0
+ */
+int add(int i, int j)
+{
+	int sum;
+
+	sum = i + j;
+	return (sum);
+}
