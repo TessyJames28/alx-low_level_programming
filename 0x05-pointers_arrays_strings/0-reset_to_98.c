cf79c38b2d35278ba5abe77f3804aa072611beee
Tessy_James <tessyjames28@gmail.com>
2022-09-19 14:28:49 -0700

    Tasks 0 reset a pointer value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - main function to reset a pointer variable
+ * @n: pointer variable of type int
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
