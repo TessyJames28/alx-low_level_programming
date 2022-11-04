757cbe8c943c4451386dec076fa015879514e670
tessyjames <tessyjames28@gmail.com>
2022-11-04 07:53:03 -0700

    function that creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file
+ * @filename: name of file to create
+ * @text_content: a NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len, count;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	for (len = 0; text_content[len]; len++)
+		;
+
+	count = write(fd, text_content, len);
+	if (count == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
